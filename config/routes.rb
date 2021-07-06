e4abe687e085f5427eade7878a493f46e0ab6ef9
Ryodai-15 <gon.4629.1208@gmail.com>
2021-07-06 08:58:21 +0000

    [Finish]kadai5

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+
+  post 'follow/:id' => 'relationships#create', as: 'follow'
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
+
+  get 'home/about' => "homes#about"
+
+
+  root to:'homes#top'
+  devise_for :users
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:new, :create, :index, :show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+
+  get '/search', to: 'searchs#search'
+
+end
